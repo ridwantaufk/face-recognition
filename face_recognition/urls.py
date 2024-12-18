a46b8f9b152394d10f5c6e6b1c15dbba81f33dfd
ridwantaufk <ridwan1998taufik@gmail.com>
2024-12-18 14:56:53 +0700

    lanjutan bag4

--- face_recognition/urls.py
+++ face_recognition/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('webcam/', views.webcam_view, name='webcam_view'),  # Halaman utama untuk webcam
-    path('webcam/feed/', views.webcam_feed, name='webcam_feed'),  # URL untuk streaming feed dari webcam
+    path('webcam/', views.webcam_view, name='webcam_view'),  # Halaman utama untuk webcam webcam
 ]
